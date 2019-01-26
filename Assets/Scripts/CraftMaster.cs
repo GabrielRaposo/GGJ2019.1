d65fe6117b3d6987d8178213df0a3953bb8bb74f
Jerry123i <igorsc.jerry@gmail.com>
2019-01-26 21:52:56 -0200

    FinishedCrafting

--- Assets/Scripts/CraftMaster.cs
+++ Assets/Scripts/CraftMaster.cs
@@ -17,7 +17,21 @@ public class CraftMaster : MonoBehaviour
 
 	public Canvas canvas;
 
+	SORecipe currentRecipe;
+
 	public List<GameObject> slots;
+
+	public Button confirmButton;
+
+	public CitizenData crafter;
+
+	public bool checkFunction;
+
+	public GameObject craftedObject;
+
+	Dictionary<string, int> dic = new Dictionary<string, int>();
+
+	
 
 	private void Awake()
 	{
@@ -36,12 +50,130 @@ public class CraftMaster : MonoBehaviour
 			s.SetActive(false);
 		}
 
+		dic.Add("w", 0);
+		dic.Add("f", 1);
+		dic.Add("e", 2);
+		dic.Add("a", 3);
+		dic.Add("wf", 4);
+		dic.Add("we", 5);
+		dic.Add("wa", 6);
+		dic.Add("fe", 7);
+		dic.Add("fa", 8);
+		dic.Add("ea", 9);
+		dic.Add("wfe", 10);
+		dic.Add("wea", 11);
+		dic.Add("wfa", 12);
+		dic.Add("fea", 13);
+		dic.Add("wfea", 14);
+
+	}
+
+	private void Update()
+	{
+		checkFunction = IsCraftReady();
+
+		if (IsCraftReady())
+			confirmButton.interactable = true;
+		else
+			confirmButton.interactable = false;
+
+	}
+
+	private bool IsCraftReady()
+	{
+		foreach (GameObject s in slots)
+		{
+			if (s.activeInHierarchy)
+			{
+				if (s.GetComponent<UISlotScript>() == null)
+				{
+					return false;
+				}
+				if (!s.GetComponent<UISlotScript>().filed)
+				{
+					return false;
+				}
+			}
+			
+
+		}
+		return true;
+	}
+
+	public void DoTheCraft()
+	{
+	
+
+		if (currentRecipe.results.Length == 1)
+		{
+			craftedObject = currentRecipe.results[0];
+			return;
+		}
+
+		int v = 0;
+		dic.TryGetValue(GetIngredientsCode(), out v);
+
+		craftedObject = currentRecipe.results[v];
+		
+		//Faz algo com o objeto
+
+	}
+
+	public string GetIngredientsCode()
+	{
+		bool w = false;
+		bool f = false;
+		bool e = false;
+		bool a = false;
+
+		foreach(GameObject s in slots)
+		{
+			if(s != null)
+				if (s.activeInHierarchy)
+				{
+					UISlotScript uISlot = s.GetComponent<UISlotScript>();
+					if(uISlot.GetType() == typeof(InspirationSlot) || uISlot.GetType() == typeof(DecorationSlot))
+					{
+						switch (uISlot.Container.type)
+						{
+							case Barks.GET_DECOR_WATER:
+								w = true;
+								break;
+							case Barks.GET_DECOR_FIRE:
+								f = true;
+								break;
+							case Barks.GET_DECOR_EARTH:
+								e = true;
+								break;
+							case Barks.GET_DECOR_AIR:
+								a = true;
+								break;
+						}
+
+					}
+				}
+		}
+
+		string key = "";
+
+		if (w)
+			key += "w";
+		if (f)
+			key += "f";
+		if (e)
+			key += "e";
+		if (a)
+			key += "a";
+
+
+		return key;
+
 	}
 
 	private void UpdateSlots(int x)
 	{
 		Debug.Log($"UPDATE SLOTS {x}");
-		SORecipe currentRecipe = recipes[x];
+		currentRecipe = recipes[x];
 
 		for(int i = 0; i < slots.Count; i++)
 		{
@@ -51,6 +183,10 @@ public class CraftMaster : MonoBehaviour
 
 				Vector3 position = slots[i].transform.position;
 				Quaternion rotation = slots[i].transform.rotation;
+
+				if(slots[i].GetComponent<UISlotScript>() != null)
+				if(slots[i].GetComponent<UISlotScript>().Container != null)
+				slots[i].GetComponent<UISlotScript>().Container.AddOne();
 
 				Destroy(slots[i]);
 
@@ -67,6 +203,10 @@ public class CraftMaster : MonoBehaviour
 						break;
 					case SlotType.INSPIRATION:
 						slots[i] = Instantiate(inspirationSlot, position, rotation, canvas.transform);
+						if(crafter != null)
+						{
+							slots[i].GetComponent<UISlotScript>().Fill(crafter);
+						}
 						break;
 				}
 				
