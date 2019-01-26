b30cd40613a0792d0adc5290778bf0b8d235ac71
Takehime <silviapimpaovasquez@hotmail.com>
2019-01-26 16:40:37 -0200

    [FEAT] Add PlayerBehaviour script

--- Assets/Scripts/PeopleBehaviour.cs
+++ Assets/Scripts/PeopleBehaviour.cs
@@ -7,19 +7,6 @@ public class PeopleBehaviour : MonoBehaviour
 
 	[SerializeField] GameObject citizenPrefab;
 
-
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
-
     public void SpawnNewcomers(int quantity) {
         for (int i = 0; i < quantity; i++) {
             GameObject newcomer = Instantiate(citizenPrefab, new Vector2(i + 1.5f, 1.5f), Quaternion.identity);
