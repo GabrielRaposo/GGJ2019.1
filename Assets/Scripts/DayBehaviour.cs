46c1b28384a46abd48071f4302a07f77c7eaacf5
Takehime <silviapimpaovasquez@hotmail.com>
2019-01-27 11:08:47 -0200

    [FIX] Random spawn points of animals in campfire scene

--- Assets/Scripts/DayBehaviour.cs
+++ Assets/Scripts/DayBehaviour.cs
@@ -5,7 +5,6 @@ using TMPro;
 
 public class DayBehaviour : MonoBehaviour
 {
-
 	public List<GameObject> interactableItemsInCurrentDay;
 	[SerializeField] GameManager gameManager;
 	[SerializeField] GameObject dayTextObject;
