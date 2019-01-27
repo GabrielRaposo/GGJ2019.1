228e8bb01f671e8549e55444b66e9a7e83bc87e1
Jerry123i <igorsc.jerry@gmail.com>
2019-01-27 15:07:52 -0200

    ItemComent

--- Assets/Scripts/CraftTileBehaviour.cs
+++ Assets/Scripts/CraftTileBehaviour.cs
@@ -42,7 +42,8 @@ public class CraftTileBehaviour : MonoBehaviour, IPointerClickHandler
         CitizenBehaviour selectedCitizenBehaviour = gameManager.SelectedCitizen.GetComponent<CitizenBehaviour>();
         selectedCitizenBehaviour.SetTurnAction(
             delegate () {
-                gameManager.GetComponent<StoryMaster>().newItemName = item.name;
+				//gameManager.GetComponent<StoryMaster>().newItemName = item.name;
+				gameManager.GetComponent<StoryMaster>().newItem = item;
             }, actions.craft);
         selectedCitizenBehaviour.OnPointerClick(null);
 
